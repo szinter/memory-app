d16796282e1c45b6b5872ab58fdcecabcc23a7df
Attila B. Gal <galaty2@gmail.com>
2018-10-23 15:54:44 +0200

    Add basic Card behaviour

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.less']
 })
 export class CardComponent implements OnInit {
+  @Input() figure: string;
+  @Input() canTurn: boolean;
+  @Input() found: boolean;
 
-  constructor() { }
+  private backSide = 'card-back';
+
+  private isTurnedOver = false;
+
+  public activeSide = this.backSide;
+
+  public turnCardOver() {
+    this.isTurnedOver = !this.isTurnedOver;
+
+    this.activeSide = this.isTurnedOver ? `card-${this.figure}` : this.backSide;
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
